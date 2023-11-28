59375e3e0bc85f8ce802c36f7d6b68b3c1e455f3
Saime Dejuwyu <acc.saime.d@gmail.com>
2023-11-28 08:24:13 +0300

    refactor

---
+++ cmd/server/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"context"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	context.WithDeadlineCause()
+	// Graceful Shutdown
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT)
+
+	<-quit
+	cancel()
+
+	time.Sleep(5 * time.Second)
+}
