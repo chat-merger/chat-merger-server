1401afc5620712e022c6cd202092648fe2a509e2
Saime Dejuwyu <acc.saime.d@gmail.com>
2023-12-02 07:42:56 +0300

    mv

--- internal/api/api.go
+++ internal/api/api.go
@@ -1,64 +1,16 @@
 package api
 
 import (
-	"chatmerger/internal/domain"
-	"chatmerger/internal/domain/model"
+	"chatmerger/pkg/mergerapi"
 )
 
-var _ domain.ClientConnectionRepository = (*ClientConnectRepositoryBase)(nil)
+var _ mergerapi.BaseServiceServer = (*Server)(nil)
 
-type ClientConnectRepositoryBase struct {
-	conns []*connect
+type Server struct {
 }
 
-func (c *ClientConnectRepositoryBase) Connect(client model.Client) (*model.ClientConnection, error) {
-	var newConn = &connect{
-		Client: client,
-		ch:     make(chan model.Message),
-	}
-	c.conns = append(c.conns, newConn)
-	return newConn.toDomain(), nil
-}
-
-func (c *ClientConnectRepositoryBase) Connected() ([]model.Client, error) {
-	var clients []model.Client
-	for _, conn := range c.conns {
-		clients = append(clients, conn.Client)
-	}
-	return clients, nil
-}
-
-func (c *ClientConnectRepositoryBase) Disconnect(id int) error {
-	for i, conn := range c.conns {
-		if conn.Id == id {
-			// remove from conns list
-			c.conns = append(c.conns[:i], c.conns[i+1:]...)
-			// close channel
-			conn.closeChan()
-		}
-	}
+func (s *Server) Connect(connService mergerapi.BaseService_ConnectServer) error {
 	return nil
 }
 
-type connect struct {
-	model.Client
-	ch chan model.Message
-}
-
-func (c *connect) closeChan() {
-	select {
-	case _, ok := <-c.ch:
-		if ok {
-			close(c.ch)
-		}
-	default:
-		close(c.ch)
-	}
-}
-
-func (c *connect) toDomain() *model.ClientConnection {
-	return &model.ClientConnection{
-		Client: c.Client,
-		MsgCh:  c.ch,
-	}
-}
+// func (s *Server) mustEmbedUnimplementedBaseServiceServer() {}
